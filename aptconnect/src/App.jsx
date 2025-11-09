1d3399fc7193fa37f6435d213a061f2bbd04a7c5
Vardhan <vardhankeerthi.dev@gmail.com>
2025-11-09 16:04:00 +0530

    Fixed tailwind plugin

--- aptconnect/src/App.jsx
+++ aptconnect/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="bg-blue-500 text-white">
-       Hi
+      <div className="bg-blue-500 h-screen flex items-center justify-center">
+       <h1 className='text-white-600 font-bold text-9xl'>Apt Connect</h1>
       </div>
     </>
   )
