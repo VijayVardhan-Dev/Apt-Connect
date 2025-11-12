0578c76008eff13f62d6f3c25ca1baff7b43f52f
GUNA_BHAI <boralakshmiprasad0@gmail.com>
2025-11-12 23:21:18 +0530

    Home

--- aptconnect/src/components/layout/DashboardLayout.jsx
+++ aptconnect/src/components/layout/DashboardLayout.jsx
@@ -3,12 +3,12 @@ import Sidebar from "./Sidebar";
 
 const DashboardLayout = ({ children }) => {
   return (
-    <div className="flex min-h-screen bg-gray-50 text-gray-900">
+    <div className="flex min-h-screen bg-white text-gray-900">
       {/* Sidebar */}
       <Sidebar />
 
       {/* Main workspace */}
-      <main className="flex-1 p-6 overflow-y-auto">{children}</main>
+      <main className="flex-1 overflow-y-auto">{children}</main>
     </div>
   );
 };
