0a5bb48d4e2e04e8f19c9d6d654c75df91d75234
Vardhan <vardhankeerthi.dev@gmail.com>
2025-11-14 01:07:26 +0530

    ui changes

--- aptconnect/src/components/layout/DashboardLayout.jsx
+++ aptconnect/src/components/layout/DashboardLayout.jsx
@@ -11,7 +11,7 @@ const DashboardLayout = ({ children }) => {
       </div>
 
       {/* Main workspace */}
-      <div className="flex-1 flex ">
+      <div className="flex-1 flex px-6 ">
         <main className="flex-1  overflow-y-auto">{children}</main>
       </div>
       
--- aptconnect/src/components/ui/ClubCard.jsx
+++ aptconnect/src/components/ui/ClubCard.jsx
@@ -10,7 +10,7 @@ export default function ClubCard({ club, onClick }) {
           alt={club.title}
           className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
         />
-        <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+        <div className="absolute inset-0 bg-linear-to-t from-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
       </div>
 
       <div className="p-5">
